aeb87d0f546e545646bbe8f1ebd10ce6d3e08a84
FreezyCGI <patrick.fritzl@gmx.at>
2023-11-17 14:49:44 +0100

    Added AzureAd credentials

--- API/API/Document/DocumentController.cs
+++ API/API/Document/DocumentController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.EntityFrameworkCore;
@@ -44,6 +45,7 @@ namespace API.Document
             return File(memory, GetContentType(documentPath), documentPath);
         }
 
+        [Authorize]
         [HttpGet]
         public Tag[] GetTags()
         {
--- API/API/Startup.cs
+++ API/API/Startup.cs
@@ -57,6 +57,11 @@ namespace API
 
             app.UseHttpsRedirection();
 
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
