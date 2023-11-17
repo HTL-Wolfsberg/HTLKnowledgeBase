c79d8ff4611630a83433da2f4dc77c10fd3d14ab
FreezyCGI <patrick.fritzl@gmx.at>
2023-11-17 14:11:35 +0100

    Moved Code from Program to Startup

--- API/API/Program.cs
+++ API/API/Program.cs
@@ -1,4 +1,5 @@
 using API.Document;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -11,59 +12,9 @@ namespace API
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-            builder.Logging.ClearProviders();
-            builder.Logging.AddDebug();
-            builder.Logging.AddConsole();
-
-            var postgresConnectionString = builder.Configuration.GetValue<string>("ConnectionStrings:postgres");
-            ConfigureServices(builder.Services, postgresConnectionString);
-
-
-            builder.Services.AddControllers();
-            builder.Services.AddMvc()
-        .AddJsonOptions(
-            options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles
-        );
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen(c =>
-            {
-                c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
-            });
-            builder.Services.Configure<KestrelServerOptions>(options =>
-            {
-                options.Limits.MaxRequestBodySize = 100 * 1000 * 1000;
-            });
-            var app = builder.Build();
-
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-
-
-            //app.UseAuthorization();
-
-            app.UseCors(x => x
-                .AllowAnyMethod()
-                .AllowAnyHeader().WithExposedHeaders("filename")
-                .SetIsOriginAllowed(origin => true) // allow any origin
-                .AllowCredentials());
-
-            app.MapControllers();
-            app.UseHttpsRedirection();
-            app.Run();
-        }
-
-        public static void ConfigureServices(IServiceCollection services, string postgresConnectionString)
-        {
-            services.AddDbContext<DocumentContext>(options =>
-                options.UseNpgsql(postgresConnectionString));
+            WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>()          
+            .Build().Run();      
         }
     }
 }
---
+++ API/API/Startup.cs
@@ -0,0 +1,66 @@
+﻿using API.Document;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+using Microsoft.Identity.Web;
+
+namespace API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var postgresConnectionString = Configuration.GetValue<string>("ConnectionStrings:postgres");
+
+            services.AddDbContext<DocumentContext>(options =>
+               options.UseNpgsql(postgresConnectionString));
+
+            services.AddControllers();
+            services.AddMvc()
+                .AddJsonOptions(
+                    options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles
+                );
+
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(c =>
+            {
+                c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+            });
+            services.Configure<KestrelServerOptions>(options =>
+            {
+                options.Limits.MaxRequestBodySize = 100 * 1000 * 1000;
+            });
+            services.AddMicrosoftIdentityWebApiAuthentication(Configuration);
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader().WithExposedHeaders("filename")
+                .SetIsOriginAllowed(origin => true) // allow any origin
+                .AllowCredentials());
+
+            app.UseHttpsRedirection();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
