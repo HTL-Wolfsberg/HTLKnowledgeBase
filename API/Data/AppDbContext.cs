db0c602fa86e32e927d14e91c546a757a5f1e3b9
FreezyCGI <patrick.fritzl@gmx.at>
2024-07-06 22:20:37 +0200

    setup asp.net project

---
+++ API/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<FileItem> FileItems { get; set; }
+        public DbSet<FileTag> FileTags { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<FileItem>()
+                .HasMany(f => f.Tags)
+                .WithOne(t => t.FileItem)
+                .HasForeignKey(t => t.FileItemId);
+        }
+    }
+
+}
---
+++ API/Models/FileItem.cs
@@ -0,0 +1,22 @@
+﻿namespace API.Models
+{
+    public class FileItem
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public List<FileTag> Tags { get; set; }
+    }
+
+    public class FileTag
+    {
+        public int Id
+        {
+            get; set;
+        }
+        public string Tag { get; set; }
+        public int FileItemId { get; set; }
+        public FileItem FileItem { get; set; }
+    }
+
+}
