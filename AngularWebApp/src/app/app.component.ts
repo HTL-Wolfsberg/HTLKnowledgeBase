62e37b88049a791376a596a3146eb7d627aad64b
FreezyCGI <patrick.fritzl@gmx.at>
2024-07-13 11:21:57 +0200

    improved navbar, added logout

--- AngularWebApp/src/app/app.component.ts
+++ AngularWebApp/src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'AngularWebApp';
+
+  constructor(public authService: AuthService, private router: Router) { }
+
+  get isAuthenticated(): boolean {
+    return this.authService.isAuthenticated();
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
