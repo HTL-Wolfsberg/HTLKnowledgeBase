5cd7bf2b6b044aa3652d3ee184b8aafc94cb9630
FreezyCGI <patrick.fritzl@gmx.at>
2024-07-14 23:27:43 +0200

    made tag filter searchable

--- AngularWebApp/src/app/components/file-list/file-list.component.ts
+++ AngularWebApp/src/app/components/file-list/file-list.component.ts
@@ -3,6 +3,9 @@ import { FileService } from '../../services/file.service';
 import { TagService } from '../../services/tag.service';
 import { FileModel } from '../../file-model';
 import { TagModel } from '../../tag-model';
+import { FormControl } from '@angular/forms';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-file-list',
@@ -14,18 +17,31 @@ export class FileListComponent implements OnInit {
   fileTypeFilter: string = '';
   selectedFilterTags: TagModel[] = [];
   availableFilterTags: TagModel[] = [];
+  filteredTags: Observable<TagModel[]>;
 
   files: FileModel[] = [];
   filteredFiles: FileModel[] = [];
 
   displayedColumns: string[] = ['name', 'size', 'type', 'tags', 'actions'];
 
-  constructor(private fileService: FileService, private tagService: TagService) { }
+  tagFilterCtrl = new FormControl();
+  private _filterTags$ = new BehaviorSubject<TagModel[]>([]);
+
+  constructor(private fileService: FileService, private tagService: TagService) {
+    this.filteredTags = this._filterTags$.asObservable();
+  }
 
   ngOnInit() {
     this.fetchFiles();
     this.tagService.getTags().subscribe((tags: TagModel[]) => {
       this.availableFilterTags = tags;
+      this._filterTags$.next(tags);
+      this.tagFilterCtrl.valueChanges
+        .pipe(
+          startWith(''),
+          map(value => this._filterTags(value))
+        )
+        .subscribe(tags => this._filterTags$.next(tags));
     });
   }
 
@@ -60,5 +76,10 @@ export class FileListComponent implements OnInit {
       window.URL.revokeObjectURL(url);
       document.body.removeChild(a);
     });
+  }
+
+  private _filterTags(value: string): TagModel[] {
+    const filterValue = value.toLowerCase();
+    return this.availableFilterTags.filter(tag => tag.name.toLowerCase().includes(filterValue));
   }
 }
